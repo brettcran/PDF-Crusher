f4a69d2767991568d7d5025b5ddc80841d650ea1
brettcran <77210142+brettcran@users.noreply.github.com>
2025-04-27 02:56:14 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,7 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('pdf-fill-sign-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/service-worker.js',
-        '/icon-192.png',
-        '/icon-512.png',
-        '/styles.css',
-        '/app.js'
-      ]);
-    })
-  );
+self.addEventListener('install', function(e) {
+  console.log('Service Worker: Installed');
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', function(e) {
+  console.log('Service Worker: Fetching');
 });
